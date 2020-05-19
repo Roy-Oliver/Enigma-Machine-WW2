6168c6a7d9bea99f88dc6e954af12a552e0c14f6
Roy-Oliver <oliverroymangosing1431@gmail.com>
2020-05-19 10:55:01 +0800

    Organized the Rotor Class

--- rotor_types.py
+++ rotor_types.py
@@ -11,17 +11,6 @@ class Rotor:
         # Moves the window position upwards. For example, A moves to B in the window. Include mod 26 to cycle through letters
         self.window_position = (self.window_position + 1) % 26
 
-class RotorI(Rotor):
-    """Models an Enigma Rotor I"""
-
-    def __init__(self, window_position, ring_setting, order):
-        super().__init__(window_position, ring_setting, order)
-
-        # Set the turnover letter
-        self.turnover = 16
-        self.wiring = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-
-
     def right_to_left(self, letter: int):
         # The following code encodes a letter from right to left (First run across Rotors)
         # Within a rotor, INFORMATION flow is PlugBoard/Right Rotor  -> Spring Contact -> Wiring ->
@@ -61,107 +50,30 @@ class RotorI(Rotor):
 
         # Return the letter
         return plugboard
+
+class RotorI(Rotor):
+    """Models an Enigma Rotor I"""
+
+    turnover = 16
+    wiring = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+
+    def __init__(self, window_position, ring_setting, order):
+        super().__init__(window_position, ring_setting, order)
 
 class RotorII(Rotor):
     """Models an Enigma Rotor II"""
 
+    turnover = 4
+    wiring = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+
     def __init__(self, window_position, ring_setting, order):
         super().__init__(window_position, ring_setting, order)
-
-        # Set the turnover letter
-        self.turnover = 4
-
-        # Set the wiring
-        self.wiring = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
-
-    def right_to_left(self, letter: int):
-        # The following code encodes a letter from right to left (First run across Rotors)
-        # Within a rotor, INFORMATION flow is PlugBoard/Right Rotor  -> Spring Contact -> Wiring ->
-        # Window (INFORMATION) -> Flat Contact
-
-        # Plugboard -> Spring Contact
-        spring_contact = (letter + self.window_position - self.ring_setting) % 26
-
-        # Spring Contact -> Wiring
-        wiring = (represent(self.wiring[spring_contact])) % 26
-
-        # Wiring -> Window (INFORMATION)
-        window = (wiring + self.ring_setting) % 26
-
-        # Window (INFORMATION) -> Flat Contact
-        flat_contact = (window - self.window_position) % 26
-
-        # Return the letter
-        return flat_contact
-
-    def left_to_right(self, letter: int):
-        # The following code encodes a letter from right to left (First run across Rotors)
-        # Within a rotor, INFORMATION flow is Flat Contact -> Window (INFORMATION) -> Wiring ->
-        # Spring Contact ->PlugBoard/Right Rotor
-
-        # Flat Contact -> Window (INFORMATION)
-        window = (letter + self.window_position) % 26
-
-        #  Window (INFORMATION) -> Wiring
-        wiring = (window - self.ring_setting) % 26
-
-        # Wiring -> Spring Contact
-        spring_contact = (self.wiring.index(represent(wiring))) % 26
-
-        # Spring Contact -> Plugboard
-        plugboard = (spring_contact - self.window_position + self.ring_setting) % 26
-
-        # Return the letter
-        return plugboard
 
 class RotorIII(Rotor):
     """Models an Enigma Rotor II"""
 
+    turnover = 21
+    wiring = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+
     def __init__(self, window_position, ring_setting, order):
         super().__init__(window_position, ring_setting, order)
-
-        # Set the turnover letter
-        self.turnover = 21
-
-        # Set the wiring
-        self.wiring = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
-
-    def right_to_left(self, letter: int):
-        # The following code encodes a letter from right to left (First run across Rotors)
-        # Within a rotor, INFORMATION flow is PlugBoard/Right Rotor  -> Spring Contact -> Wiring ->
-        # Window (INFORMATION) -> Flat Contact
-
-        # Plugboard -> Spring Contact
-        spring_contact = (letter + self.window_position - self.ring_setting) % 26
-
-        # Spring Contact -> Wiring
-        wiring = (represent(self.wiring[spring_contact])) % 26
-
-        # Wiring -> Window (INFORMATION)
-        window = (wiring + self.ring_setting) % 26
-
-        # Window (INFORMATION) -> Flat Contact
-        flat_contact = (window - self.window_position) % 26
-
-        # Return the letter
-        return flat_contact
-
-    def left_to_right(self, letter: int):
-        # The following code encodes a letter from right to left (First run across Rotors)
-        # Within a rotor, INFORMATION flow is Flat Contact -> Window (INFORMATION) -> Wiring ->
-        # Spring Contact ->PlugBoard/Right Rotor
-
-        # Flat Contact -> Window (INFORMATION)
-        window = (letter + self.window_position) % 26
-
-        #  Window (INFORMATION) -> Wiring
-        wiring = (window - self.ring_setting) % 26
-
-        # Wiring -> Spring Contact
-        spring_contact = (self.wiring.index(represent(wiring))) % 26
-
-        # Spring Contact -> Plugboard
-        plugboard = (spring_contact - self.window_position + self.ring_setting) % 26
-
-        # Return the letter
-        return plugboard
